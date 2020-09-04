10183791be993292d12a9f1f29a731eaf7f79a5a
bronsonbdevost <bronsonbdevost@aim.com>
2020-09-04 08:46:21 +0000

    gave user test@1.com all system roles

---
+++ Changes/0.20.4/db-updates-0.20.4.sql
@@ -0,0 +1,6 @@
+##########################################################
+## Add fole system roles permissions to user test@1.com ##
+##########################################################
+
+INSERT INTO users_system_roles (user_id,system_roles_id)
+VALUES (2,2), (2,3), (2,4);
