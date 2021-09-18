336794e5853df6c2815c77a27d9fb82eba8d0fec
Bronson Brown-deVost <bronsonbdevost@gmail.com>
2021-09-18 13:22:15 +0200

    artefact_shape_owner table patch

---
+++ Changes/0.25.2/db-updates-0.25.2.sql
@@ -0,0 +1,24 @@
+START TRANSACTION;
+
+-- Set the version of this update (CURRENT_DATABASE_VERSION)
+SELECT @VER := "0.25.0";
+
+INSERT INTO `db_version` (version)
+VALUES (@VER);
+
+-- Insert missing artefact_shape_owner table entries
+insert into artefact_shape_owner (artefact_shape_id, edition_editor_id, edition_id)
+SELECT DISTINCT artefact_shape.artefact_shape_id, artefact_data_owner.edition_id, artefact_data_owner.edition_id
+from artefact_shape
+join artefact_data USING(artefact_id)
+join artefact_data_owner using(artefact_data_id)
+left join artefact_shape_owner using(artefact_shape_id)
+where artefact_shape_owner.edition_id is null and artefact_data_owner.edition_id < 1646
+
+
+-- Record the completion of the update
+UPDATE `db_version`
+SET completed = current_timestamp()
+WHERE version = @VER;
+
+COMMIT;
