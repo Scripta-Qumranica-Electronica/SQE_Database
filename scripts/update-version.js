82ffc919292ce3165064e36eec0cf689dac7a58b
Hannu Kalavainen <ridge.rb.batty@gmail.com>
2021-11-25 11:31:56 +0200

    Removed gzip compression

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -6,7 +6,7 @@ const chalk = require('chalk')
 const fs   = require('fs')
 const { runMain } = require('module')
 
-const run = (shellCmd, params) => {         // shellCmd:String, params:String > { status:String, msg:String }
+const run = (shellCmd, params) => {         // shellCmd:String, params:String > { status:String, msg:String }git 
     return new Promise((resolve, reject) => {
         exec(shellCmd + ' ' + params, (error, stdout, stderr) => {
             if (error) {
@@ -15,7 +15,7 @@ const run = (shellCmd, params) => {         // shellCmd:String, params:String > 
             resolve({ status:'ok', msg:stdout.toString(), stderr });
         });
     }).catch(e => {
-        log(e);
+        console.log(e);
     });
 }
 
@@ -27,43 +27,36 @@ if (!args.t) {
 
 console.log(args);
 
-(async _=> {
-	try {
-		const package = require(filename)
-		fs.writeFileSync(filename, JSON.stringify({...package, version: args.t}, null, 2) , 'utf-8')
-		console.log(chalk.green(`✓ Successfully updated package.json version to ${args.t}.`))
-	} catch (err) {
-		console.error(chalk.red(`✗ Could not updated package.json version to ${args.t}.`))
-		console.log(err) 
-		process.exit(1)
+try {
+	const package = require(filename)
+	fs.writeFileSync(filename, JSON.stringify({...package, version: args.t}, null, 2) , 'utf-8')
+	console.log(chalk.green(`✓ Successfully updated package.json version to ${args.t}.`))
+} catch (err) {
+	console.error(chalk.red(`✗ Could not updated package.json version to ${args.t}.`))
+	console.log(err) 
+	process.exit(1)
+}
+
+let sqlUpdateFile = `Changes/${args.t}/db-updates-${args.t}.sql`
+
+try {	
+	let sqlString = fs.readFileSync(sqlUpdateFile, 'utf8')
+	if (!sqlString.startsWith('START TRANSACTION;') && args['wrap-SQL']) {
+		// Wrap the SQL string with code required to update a version
+		sqlString = 'START TRANSACTION;\n' + sqlString + 
+					`SELECT @VER := "${args.t}";\n` +
+					'INSERT INTO `db_version` (version, completed) VALUES (@VER, current_timestamp());\n' +
+					'COMMIT;'
+		console.log(chalk.green(`✓ Wrapped SQL at ${sqlUpdateFile}`));
+	} else {
+		sqlString = sqlString.replace(/\$CURRENT_DATABASE_VERSION/g, args.t);
 	}
-
-	let sqlUpdateFile = `Changes/${args.t}/db-updates-${args.t}.sql`
-
-	try {
-		if (!fs.existsSync(sqlUpdateFile)) throw 'File not found:' + sqlUpdateFile;				// if the sql script file is gzipped, this would've exploded
-
-		let sqlString = fs.readFileSync(sqlUpdateFile, 'utf8')
-		if (!sqlString.startsWith('START TRANSACTION;') && args['wrap-SQL']) {
-			// Wrap the SQL string with code required to update a version
-			sqlString = 'START TRANSACTION;\n' + sqlString + 
-						`SELECT @VER := "${args.t}";\n` +
-						'INSERT INTO `db_version` (version, completed) VALUES (@VER, current_timestamp());\n' +
-						'COMMIT;'
-			console.log(chalk.green(`✓ Wrapped SQL at ${sqlUpdateFile}`));
-		} else {
-			sqlString = sqlString.replace(/\$CURRENT_DATABASE_VERSION/g, args.t);
-		}
-		fs.writeFileSync(sqlUpdateFile, sqlString, 'utf-8')
+	fs.writeFileSync(sqlUpdateFile, sqlString, 'utf-8')
 		
-		console.log('Zipping the SQL file...');
-		await run('gzip', sqlUpdateFile);
-		
-		console.log(chalk.green(`✓ Successfully updated ${sqlUpdateFile} database version to ${args.t}.`))    
-	} catch (err) {
-		console.error(chalk.red(`✗ Could not update the file ${sqlUpdateFile} to database version to ${args.t}.`))
-		console.log(err)
-	}
+	console.log(chalk.green(`✓ Successfully updated ${sqlUpdateFile} database version to ${args.t}.`))    
+} catch (err) {
+	console.error(chalk.red(`✗ Could not update the file ${sqlUpdateFile} to database version to ${args.t}.`))
+	console.log(err)
+}
 
-	process.exit(0)
-})();
+process.exit(0)
